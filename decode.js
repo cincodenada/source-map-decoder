05e5259aaf202ebd0dcc54cf110d3ab18ae1be8d
Joel Bradshaw <joelbr@tailormed.co>
2022-04-19 18:40:54 -0700

    Add decoding

--- decode.js
+++ decode.js
@@ -30,9 +30,14 @@ class SourceMapDecoder extends Writable {
       if (!map.mappings) throw new Error('source map had no "mappings" property')
       if (!Array.isArray(map.sources)) throw new Error('source map had no "sources" property')
       if (!Array.isArray(map.names)) throw new Error('source map had no "names" property')
+
+      const newMappings = (typeof map.mappings === 'string')
+        ? decodeMappings(map.mappings, map.sources, map.names)
+        : encodeMappings(map.mappings, map.sources, map.names)
+
       return JSON.stringify({
         ...map,
-        mappings: formatMappings(map.mappings, map.sources, map.names)
+        mappings: newMappings
       })
     } catch (e) {
       this.emit('error', e)
@@ -42,16 +47,16 @@ class SourceMapDecoder extends Writable {
 
 module.exports = SourceMapDecoder
 
-const formatMappings = (mappings, sources, names) => {
+function decodeMappings(mappings, sources, names) {
   const vlqState = [ 0, 0, 0, 0, 0 ]
   return mappings.split(';').reduce((accum, line, i) => {
-    accum[i + 1] = formatLine(line, vlqState, sources, names)
+    accum[i + 1] = decodeLine(line, vlqState, sources, names)
     vlqState[0] = 0
     return accum
   }, {})
 }
 
-const formatLine = (line, state, sources, names) => {
+function decodeLine(line, state, sources, names) {
   const segs = line.split(',')
   return segs.map(seg => {
     if (!seg) return ''
@@ -59,9 +64,36 @@ const formatLine = (line, state, sources, names) => {
     for (var i = 0; i < 5; i++) {
       state[i] = typeof decoded[i] === 'number' ? state[i] + decoded[i] : state[i]
     }
-    return formatSegment(...state.concat([ sources, names ]))
+    return decodeSegment(...state.concat([ sources, names ]))
   })
 }
 
-const formatSegment = (col, source, sourceLine, sourceCol, name, sources, names) =>
-  `${col + 1} => ${sources[source]} ${sourceLine + 1}:${sourceCol + 1}${names[name] ? ` ${names[name]}` : ``}`
+function decodeSegment(col, source, sourceLine, sourceCol, name, sources, names) {
+  return `${col + 1} => ${sources[source]} ${sourceLine + 1}:${sourceCol + 1}${names[name] ? ` ${names[name]}` : ``}`
+}
+
+function encodeMappings(mappings, sources) {
+  const vlqState = Array(4).fill(0)
+  return Object.values(mappings).map(line => encodeLine(line, vlqState, sources)).join(';')
+}
+
+function encodeLine(line, sources, state) {
+  return line.map(seg => encodeSegment(seg, sources, state)).join(',')
+}
+
+function encodeSegment(segment, sources, state) {
+  // TODO: Deal with names
+  let parts = segment.split(' ')
+  const mapCol = parts[0]
+  const filename = parts.slice(2,-2).join(' ')
+  const [sourceLine, sourceCol] = parts[parts.length-1].split(':')
+
+  const sourceIdx = sources.indexOf(filename)
+  const values = [mapCol-1, sourceIdx, sourceLine-1, sourceCol-1]
+  const relativeValues = values.map((val, idx) => {
+    const diff = val - state[idx];
+    state[idx] = val;
+    return diff
+  })
+  return vlq.encode(relativeValues)
+}
